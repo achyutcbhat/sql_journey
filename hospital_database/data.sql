578e3df61f027b35326de49685521d7db38abca7
Achyut Bhat <achyutbhat5@gmail.com>
2026-01-29 22:13:13 +0530

    Create data.sql

---
+++ hospital_database/data.sql
@@ -0,0 +1,28 @@
+-- inserting the data on the tables 
+
+-- datas of patients 
+insert into patients values
+                         (1, 'Arjun Kumar', 30, 'Male', 'Diabetes', 'Chennai', 12000),
+                         (2, 'Priya Sharma', 30, 'Female', 'Fever', 'Delhi', 3000),
+                         (3, 'Rahul Verma', 45, 'Male', 'Heart Disease', 'Mumbai', 85000),
+                         (4, 'Sneha Iyer', 30, 'Female', 'Migraine', 'Chennai', 7000),
+                         (5, 'Amit Patel', 45, 'Male', 'Blood Pressure', 'Ahmedabad', 15000),
+                         (6, 'Kavya Reddy', 28, 'Female', 'Anemia', 'Hyderabad', 5000),
+                         (7, 'Suresh Nair', 45, 'Male', 'Arthritis', 'Mumbai', 22000),
+                         (8, 'Neha Singh', 34, 'Female', 'Asthma', 'Delhi', 11000),
+                         (9, 'Rohit Mehta', 30, 'Male', 'Kidney Stone', 'Pune', 40000),
+                         (10, 'Pooja Das', 28, 'Female', 'Skin Allergy', 'Kolkata', 6500);
+
+-- data of  doctors 
+insert into doctors values
+                        (101, 'Dr. Anil Rao', 'Cardiologist', 15, 'Mumbai'),
+                        (102, 'Dr. Meena Gupta', 'Dermatologist', 10, 'Delhi'),
+                        (103, 'Dr. Ramesh Iyer', 'Neurologist', 15, 'Chennai'),
+                        (104, 'Dr. Sunita Sharma', 'Gynecologist', 10, 'Delhi'),
+                        (105, 'Dr. Vikram Patel', 'Orthopedic', 20, 'Ahmedabad'),
+                        (106, 'Dr. Nikhil Verma', 'Physician', 8, 'Noida'),
+                        (107, 'Dr. Priyanka Nair', 'Pediatrician', 10, 'Kochi'),
+                        (108, 'Dr. Sanjay Malhotra', 'Nephrologist', 15, 'Mumbai'),
+                        (109, 'Dr. Kavita Joshi', 'ENT Specialist', 12, 'Pune'),
+                        (110, 'Dr. Arvind Mishra', 'General Surgeon', 8, 'Bhopal');
+
