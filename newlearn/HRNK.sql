0564e67988512c8114b00e5bc25a88e099ad8000
Achyut Bhat <achyutbhat5@gmail.com>
2026-01-31 21:34:49 +0530

    Add SQL queries for CITY names with vowel conditions
    Added SQL queries to retrieve CITY names starting and ending with vowels from STATION.

--- newlearn/HRNK.sql
+++ newlearn/HRNK.sql
@@ -1,4 +1,5 @@
 /*
+30/01/2026 
 Questions- 1
             Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
 
@@ -11,5 +12,20 @@ query for the listing == */
               SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[AEIOUaeiou]';
 
 -- no need to mention both capital and small later string to list.because SQL  is case insensitive 
+
+/* 
+31/01/2026
+Question- 2 
+            Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+This question is same like a Q1, but we have to find the words ends with vowels. but the query is little bit different 
+*/
+            SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '[AEIOUaeiou]$';
+
+/* HERE you can see the difference bitween the two questions.
+for find the word start with, you should use  REGEXP '^[character you want]'; but same to find the word end with, you will  going to use $ instead of ^ at the end 
+like REGEXP '[character you want]$'; 
+*/
+
 
 
