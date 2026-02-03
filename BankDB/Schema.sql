f14b6e80b8e05d856aed93be3237cd0a6f6d6786
Achyut Bhat <achyutbhat5@gmail.com>
2026-02-03 22:00:59 +0530

    Add initial database schema for bankdb
    Created SQL schema for bank database with account and loans tables.

---
+++ BankDB/Schema.sql
@@ -0,0 +1,23 @@
+-- create the sql data base on name bankdb 
+    create database bankdb;
+    use bankdb;
+
+-- create the table name account
+    create table account(
+        accountNO int primary key ,
+        name varchar(100) not null ,
+        branch varchar(100),
+        city varchar(100),
+        balance int,
+        accounttype varchar(50),
+        opendate varchar(50)
+    );
+
+-- create the table name loans 
+    create table loans(
+        loanid int primary key ,
+        accountNO int not null ,
+        loantype varchar(50),
+        amount int,
+        issuedate varchar(50)
+    );
