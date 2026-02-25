d89860de3401736a11fd4ba1aada9de8d7f809bc
Achyut Bhat <achyutbhat5@gmail.com>
2026-02-25 23:25:28 +0530

    Update hackerrankQNS.sql

--- newlearn/hackerrankQNS.sql
+++ newlearn/hackerrankQNS.sql
@@ -153,6 +153,18 @@ so we will use floor() which is It rounds a number DOWN to the nearest whole num
                                                                                              3.99      3 
 
 */ 
+/* 25/02/2026 
+QNS -- 
+Consider P1(a,b) and p2(c,d)  to be two points on a 2D plane.
+
+ a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points p1 and p2  and round it to a scale of 4 decimal places
+*/ 
+SELECT ROUND ((MAX(LAT_N)-MIN(LAT_N)) + (MAX(LONG_W)-MIN(LONG_W)), 4) FROM STATION; 
+
 
 
 
